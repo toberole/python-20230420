33d961745b2a68f3716b60ec6e77ac5cc180514c
zhouwei <zhouwei10@zhouweideMacBook-Pro.local>
2023-05-06 14:34:53 +0800

    note

---
+++ test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+# 导入 math 包
+import math
+
+# arr = [1, 1, 2, 3, 4, 5]
+# print(arr.count(1))
+
+# n = 5
+# n -= (0 if 0 == 0 else 1)
+# print(n)
+
+# n = 1 - \
+#     2
+# print(n)
+
+# print("hello world!")
+
+# if 1:
+#     print("True")
+
+# if 0:
+#     print("False")
+
+str = 'Hello World!'
+print(str)
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str * 2)
+print(str + "TEST")
